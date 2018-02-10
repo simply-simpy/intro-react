cc8b0d7c62d42e536d7271515460a0a73d799e18
Scott Simpson <>
2018-02-10 12:49:52 -0800

    professions dynamically generated

--- finished/state-two-components/src/App.js
+++ finished/state-two-components/src/App.js
@@ -10,55 +10,61 @@ class App extends Component {
           name: "John Doe",
           avatar: "http://i.pravatar.cc/150?img=3",
           profession: "banker",
-          city: "LA"
+          city: "LA",
+          email: "john@gmail.com"
         },
         {
           name: "Sally Smith",
           avatar: "http://i.pravatar.cc/150?img=45",
           profession: "attorney",
-          city: "NYC"
+          city: "NYC",
+          email: "sally@gmail.com"
         },
         {
           name: "Fred Jones",
           avatar: "http://i.pravatar.cc/150?img=51",
           profession: "computer programmer",
-          city: "Portland"
+          city: "Portland",
+          email: "fres@gmail.com"
         },
         {
           name: "Linda Williams",
           avatar: "http://i.pravatar.cc/150?img=44",
           profession: "Doctor",
-          city: "Atlanta"
+          city: "Atlanta",
+          email: "linda@gmail.com"
         },
         {
           name: "Piff Jenkins",
           avatar: "http://i.pravatar.cc/150?img=57",
           profession: "Doctor",
-          city: "LA"
+          city: "LA",
+          email: "piff@gmail.com"
         },
         {
           name: "Eleanor Fant",
           avatar: "http://i.pravatar.cc/150?img=48",
           profession: "attorney",
-          city: "NYC"
+          city: "NYC",
+          email: "eleanor@gmail.com"
         },
         {
           name: "Hanson Deck",
           avatar: "http://i.pravatar.cc/150?img=13",
           profession: "Doctor",
-          city: "Portland"
+          city: "Portland",
+          email: "hanson@gmail.com"
         },
         {
           name: "Hilary Ouse",
           avatar: "http://i.pravatar.cc/150?img=9",
           profession: "Doctor",
-          city: "Atlanta"
+          city: "Atlanta",
+          email: "hilary@gmail.com"
         }
       ],
       selectedCandidates: []
     };
-
-
   }
 
   componentDidMount() {
@@ -69,39 +75,81 @@ class App extends Component {
     )
   }
 
-
   render() {
-    const name = {display: 'block'};
-    const peopleProfession = {display: 'block'};
-    const selectPerson = {
+    const professionStyle = {display: 'block'};
+    const itemStyle = {display: 'block'};
+    const peopleProfessionStyle = {display: 'block'};
+    const avatarProfessionStyle = {
       display: 'flex',
-      maxWidth: '50%',
+      maxWidth: '375px',
       overflowX: 'scroll',
       margin: '10px auto'
     };
+    const fullInfoStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      maxWidth: '375px',
+      overflowX: 'scroll',
+      margin: 'auto',
+      backgroundColor: '#eee',
+      border: '1px solid #333',
+      padding: '10px',
+      lineHeight: '1.3'
+    };
+    const labelStyle = {
+      display: 'block'
+    };
 
-    let candidates = this.state.candidates.map((candidate, index) =>
-        <div className="people-profession" key={index}>
-          <span style={peopleProfession} className="avatar"><img src={candidate.avatar} alt=""/></span>
-          <span style={name} className="name">{candidate.profession}</span>
+    let avatarProfession = this.state.selectedCandidates.map((candidate, index) =>
+        <div className="avatar-profession" key={index}>
+          <span style={peopleProfessionStyle} className="avatar"><img src={candidate.avatar} alt=""/></span>
+          <span style={professionStyle} className="profession">{candidate.profession}</span>
         </div>
     );
 
+    let fullInfo = this.state.selectedCandidates.map((candidate, index) =>
+        <div style={fullInfoStyle} key={index}>
+          <span style={itemStyle} className="profession">{candidate.name}</span>
+          <span style={itemStyle} className="profession">{candidate.profession}</span>
+          <span style={itemStyle} className="profession">{candidate.email}</span>
+          <span style={itemStyle} className="profession">{candidate.city}</span>
+        </div>
+    );
 
+    let filteredProfessions = [];
+    this.state.candidates.map((candidate) =>
+        filteredProfessions.push(candidate.profession)
+    );
+    filteredProfessions = [...new Set(filteredProfessions)]; // removes duplicates
+    let professions = filteredProfessions.map((profession, index) =>
+            <label key={index}>
+              <input type="radio" id={index} name="candidate.profession" value="candidate.profession"/>
+              <span className="label">{profession.profession}</span>
+            </label>
+        );
+    console.log("filtered 2: ", filteredProfessions)
+    // filteredLocations.push(location);
+    // filteredLocations = [...new Set(filteredLocations)]; // removes duplicates
     return (
         <div className="App">
-          <div style={selectPerson} className="select-person">
-            {candidates}
+          <h1>Job Candidate Search</h1>
+          <div style={avatarProfessionStyle} className="avatar-profession">
+            {avatarProfession}
+          </div>
+          <div className="filter-by-profession">
+            {professions}
+          </div>
+          <div className="full-info-wrap">
+            {fullInfo}
           </div>
 
-          <div style={{position: 'fixed', bottom: 20+"px"}}className="state">
-          Selected Candidates:<br />
+          {/*shows the updating of selected candidates*/}
+          <div style={{marginTop: 30 + "px"}} className="state">
+            Selected Candidates:<br/>
             <code style={{fontWeight: 'bold'}}>
-            {
-              this.state.selectedCandidates.map((selectedCandidates) =>
+              {this.state.selectedCandidates.map((selectedCandidates) =>
                   JSON.stringify(selectedCandidates)
-              )
-            }
+              )}
             </code>
           </div>
         </div>
